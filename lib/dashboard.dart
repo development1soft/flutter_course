a2ea083eea657f5ffc9808f98ef8c07b904cdc08
Eyad Web Developmnet <30596418+development1soft@users.noreply.github.com>
2020-02-28 23:08:19 +0300

    Sign In With Firebse

---
+++ lib/dashboard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class DashboardScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Container(
+        child: Text('Welcome To Your Dashboard'),
+      ),
+    );
+  }
+  
+}
+
---
+++ lib/login.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_with_firebase/dashboard.dart';
+
+class LoginScreen extends StatefulWidget{
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  void initState()
+  {
+    super.initState();
+  }
+
+
+  final _formkey = GlobalKey<FormState>();
+
+  TextEditingController _emailcontroller = TextEditingController();
+
+  TextEditingController _passwordcontroller = TextEditingController();
+
+  @override
+  void dispose()
+  {
+    _emailcontroller.dispose();
+
+    _passwordcontroller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login To My Account'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Form(
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _emailcontroller,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (value){
+                    if(value.isEmpty){
+                      return 'Please Fill Email Input';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: _passwordcontroller,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  validator: (value){
+                    if(value.isEmpty){
+                      return 'Please Fill Password Input';
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                RaisedButton(
+                  color: Colors.blue,
+                  child: Text('Login',style: TextStyle(color: Colors.white),),
+                  onPressed: () async {
+                    if(_formkey.currentState.validate()){
+
+                      var result = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _emailcontroller.text, password: _passwordcontroller.text);
+
+                      if(result != null){
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => DashboardScreen()),
+                        );
+                      }else{
+                        print('user not found');
+                      }
+                    }
+                  },
+                )
+
+              ],
+            )
+        ),
+      ),
+    );
+  }
+
+
+
+}
